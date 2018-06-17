cbf958bc12b1d25f8ca6be9f46469986f4173100
Réal <rgoddat@gmail.com>
2018-06-17 23:46:40 +0200

    Created fish prefab

---
+++ Assets/Noot/Scripts/Player/PickFish.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PickFish : MonoBehaviour
+{
+    public AudioClip SoundFishPick;
+    private ScoreManager scoreManager;
+
+    private void Start()
+    {
+        scoreManager = GameObject.Find("FPSController").GetComponent<ScoreManager>();
+    }
+
+    private void OnTriggerEnter(Collider collision)
+    {
+        Debug.Log("collision with" + collision.gameObject.name);
+        if(collision.gameObject.tag == "Fish")
+        {
+            StartCoroutine(DestroyFish(collision.gameObject));
+        }
+    }
+
+    private IEnumerator DestroyFish(GameObject fish)
+    {
+        fish.GetComponent<AudioSource>().PlayOneShot(SoundFishPick);
+        scoreManager.AddScore();
+        yield return new WaitForSeconds(0.5f);
+        Destroy(fish);
+    }
+}
---
+++ Assets/Noot/Scripts/Player/ScoreManager.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ScoreManager : MonoBehaviour {
+
+    public Text TxtScore;
+
+    private float score = 0;
+
+    private void Start()
+    {
+        TxtScore.text = score.ToString();
+    }
+
+    public void AddScore()
+    {
+        score++;
+        TxtScore.text = score.ToString();
+    }
+
+}
