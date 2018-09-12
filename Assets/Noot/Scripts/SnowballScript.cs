aba80c510fe684370111528239766fdfe3d90d55
Réal <rgoddat@gmail.com>
2018-09-12 18:23:54 +0200

    Back to desktop

---
+++ Assets/Noot/Scripts/SnowballScript.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SnowballScript : MonoBehaviour {
+
+    public float LifeSpan = 1f;
+    public AudioClip SoundHit;
+
+    private AudioSource audioSource;
+
+	// Use this for initialization
+	void Start () {
+        audioSource = GetComponent<AudioSource>();
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        audioSource.PlayOneShot(SoundHit);
+        Destroy(gameObject, LifeSpan);
+    }
+}
